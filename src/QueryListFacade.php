02fa04279073fde38e8d81a39f123c60b60bb2be
Roy688 <slaythia@gmail.com>
2018-02-09 09:15:18 +0800

    First Commit

---
+++ src/QueryListFacade.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gold688\QueryList;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Roy688\QueryList\QueryList
+ */
+class QueryListFacade extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'querylist';
+    }
+}
---
+++ src/QueryListProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Roy688\QueryList;
+
+use Roy688\QueryList\QueryList;
+use Illuminate\Support\ServiceProvider;
+
+class QueryListProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerQueryList();
+
+        $this->registerAliases();
+    }
+
+    /**
+     * Register the QueryList instance.
+     *
+     * @return void
+     */
+    protected function registerQueryList()
+    {
+        $this->app->singleton('querylist', function ($app) {
+            return new \QL\QueryList();
+        });
+    }
+
+    /**
+     * Register class aliases.
+     *
+     * @return void
+     */
+    protected function registerAliases()
+    {
+      $this->app->alias('querylist', 'QL\QueryList');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [ 'querylist' ];
+    }
+}
